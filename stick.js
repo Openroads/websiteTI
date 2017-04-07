cce3a9f2ddd42be668c7ba89d4a51e2a95b5fb0d
daren <daren22@o2.pl>
2017-04-07 20:30:01 +0200

    poprawki

--- stick.js
+++ stick.js
@@ -1,11 +1,16 @@
 	$(document).ready(function() {
+
 	var NavY = $('.menu').offset().top;
 	var stickyNav = function(){
 	var ScrollY = $(window).scrollTop();
-		  
+	var t = NavY-ScrollY;
+	var top = t +"px";
+	//top = "10px";
+
 	if (ScrollY > NavY) { 
 		$('.menu').addClass('sticky');
 	} else {
+		//$('.menu').css({"top":top});
 		$('.menu').removeClass('sticky'); 
 	}
 	};
